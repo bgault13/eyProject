9eb1f432f2d304a6a5c143db814422d74738c35d
Kyle Bradley <kj.bradley987@gmail.com>
2019-03-11 15:12:35 -0400

    Add files via upload
    Cleaned some stuff up

--- marketwatch.py
+++ marketwatch.py
@@ -7,12 +7,10 @@ Created on Sat Mar  9 11:00:34 2019
 """
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
 from bs4 import BeautifulSoup
-import re
-import pandas as pd
-from tabulate import tabulate
-import os
+
+
 
 #launch url
 url = "https://www.marketwatch.com/search?q=KPMG&m=Keyword&rpp=15&mp=806&bd=false&rs=false&o=16"
@@ -22,17 +20,16 @@ driver = webdriver.Firefox()
 driver.implicitly_wait(30)
 driver.get(url)
 
-#After opening the url above, Selenium clicks the specific agency link
 
 
-#Selenium hands the page source to Beautiful Soup
+
 soup_level1=BeautifulSoup(driver.page_source, 'lxml')
 
 datalist = [] #empty list
 x = 0 #counter
 
-#Beautiful Soup finds all Job Title links on the agency page and the loop begins
-for x in range(20):
+#sometimes this breaks
+for x in range(50):
     for link in driver.find_elements_by_xpath('//a[@href]'):
         if 'www.marketwatch.com/story' in link.get_attribute('href'):
                 datalist.append(link.get_attribute('href'))
